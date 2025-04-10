f6652f5a4b9e6a9cd796c2a31adbdb7c20824b71
Simen Bihaug-Frøyland <sbfroyland@gmail.com>
2025-04-10 18:00:30 +0200

    Exclude 'rouge' package from setup.py package discovery

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="Summarization Project",
     version="0.1",
-    packages=find_packages(),  
+    packages=find_packages(exclude=["rouge"]),
     install_requires=[], 
 )
